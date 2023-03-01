199632237d9e0cc9260935df063401344c692ff3
KJ <kjloving1400@gmail.com>
2023-02-28 17:39:11 -0800

    first round of validations

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,3 +1,8 @@
 class Availability < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :start_time, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\z/ }
+  validates :end_time, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\z/ }, date: { after: :start_time }
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :availabilities
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/ }
+    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :phone_number, presence: true, length: { minimum: 10, maximum: 15 }, format: { with: /\A[0-9]+\z/ }
 end
